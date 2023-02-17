779e3bc29d2417e02c4e75fbbfe3be1a87521879
Aditya <ask2adi007@gmail.com>
2023-02-18 01:12:59 +0530

    Added more tree problems
    Change-Id: I687681461830012bb1657939da38f1e1b9c772cf

---
+++ src/dataStructure/BinaryTree/ZigZagTraversal.java
@@ -0,0 +1,14 @@
+package dataStructure.BinaryTree;
+import java.util.*;
+
+public class ZigZagTraversal {
+	public List<List<Integer>> zigZagTraversal(TreeNode root)
+	{
+		List<List<Integer>> ans = new ArrayList<List<Integer>>();
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		q.add(root);
+		
+		
+		return ans;
+	}
+}
